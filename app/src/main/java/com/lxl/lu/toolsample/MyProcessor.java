bc4404a8b244fcfa5a1ee4fd711720167106aa60
lyw <648422720@qq.com>
2017-08-13 22:38:27 +0800

    1 添加gradle 全局配置

---
+++ app/src/main/java/com/lxl/lu/toolsample/MyProcessor.java
@@ -0,0 +1,9 @@
+package com.lxl.lu.toolsample;
+
+
+/**
+ * Created by lyw on 2017/8/13.
+ */
+
+public class MyProcessor  {
+}
---
+++ tools-annotations/src/main/java/com/example/tools_annotations/myClass.java
@@ -0,0 +1,4 @@
+package com.example.tools_annotations;
+
+public class myClass {
+}
